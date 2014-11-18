09ddb8291f349c8f943057855610ce7e43ad4d17
joshsalvi <joshsalvi@jds-mbp.rockefeller.edu>
2014-11-18 13:16:13 -0500

    updated importVLCdata2
    pre/pulse error fixed

--- importVLCdata2.m
+++ importVLCdata2.m
@@ -27,8 +27,10 @@ a = length(file);       % number of sessions in the directory
 logdata = importdata(sprintf('%s%s',datapath,logfile.name));  % logdata.data contains log data of interest
 comments = logdata.textdata(isnan(logdata.data(:,8))==0,3); % import comments
 Fs = logdata.data(1,12);       % scan rate (Hz), CHECK THIS!
-pre = logdata.data(1,22)*1e-3*Fs;   % delay before a stimulus, CHECK THIS!
-pulse = logdata.data(1,23)*1e-3*Fs; % length of stimulus, CHECK THIS!
+for j = 1:a
+pre(j) = logdata.data(j,22)*1e-3*Fs;   % delay before a stimulus, CHECK THIS!
+pulse(j) = logdata.data(j,23)*1e-3*Fs; % length of stimulus, CHECK THIS!
+end
 % Note that some logfiles will have formatting issues and the indices
 % listed above may be off by ±10 in certain cases. If this is true, open
 % logdata.data and modify the script accordingly.
@@ -65,13 +67,15 @@ for j = nonraw
 end
 % Extract pulses from full time traces
 k=1;
+
 for j = nonraw
     for i = 1:ntrace(k)
-        Xd_pulse{i,j} = Xd{i,j}(1+pre:pre+pulse);  % photodiode, stimulation only
-        Xo_pulse{i,j} = Xo{i,j}(1+pre:pre+pulse);  % stimulus piezo, stimulation only
+        Xd_pulse{i,j} = Xd{i,j}(1+pre(j):pre(j)+pulse(j));  % photodiode, stimulation only
+        Xo_pulse{i,j} = Xo{i,j}(1+pre(j):pre(j)+pulse(j));  % stimulus piezo, stimulation only
     end
     k=k+1;
 end
+
 % Import non-raw time traces
 k=1;
 for j = raw
